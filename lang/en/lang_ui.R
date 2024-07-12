ad7dd0d1dad114cd45974f89d44986de9fe94988
Helen Lam <49119473+Helenlam20@users.noreply.github.com>
2024-07-12 14:19:43 +0200

    Update lang_ui.R

--- lang/en/lang_ui.R
+++ lang/en/lang_ui.R
@@ -1,6 +1,6 @@
 # Dashboard
 lang[["title"]] <- "The Opportunity Gap in the Netherlands"
-lang[["title_width"]] <- 400
+lang[["title_width"]] <- 500
 
 # General
 lang[["outcome_measure"]] <- "Outcome"
